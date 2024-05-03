b84221a4d937ebb5640bcb7fb0849d56b864411a
Sean Wong <seanwong24@live.com>
2024-05-03 08:19:10 +0000

    separate data structures

--- gwfvis/db/__init__.py
+++ gwfvis/db/__init__.py
@@ -1,83 +1,13 @@
-
 # %% imports
 from dataclasses import dataclass
 import json
 import os
 import sqlite3
 from typing import Dict, Sequence
-
-# %% data structures
-
-
-@dataclass
-class Info:
-    key: str
-    value: str = None
-    label: str = None
-
-    def __hash__(self):
-        return id(self)
-
-
-@dataclass
-class Location:
-    id: int
-    geometry: dict
-    metadata: dict = None
-
-    def __hash__(self):
-        return id(self)
-
-
-@dataclass
-class Dimension:
-    id: int
-    name: str
-    size: int
-    description: str = None
-    value_labels: Sequence[str] = None
-
-    def __hash__(self):
-        return id(self)
-
-
-@dataclass
-class Variable:
-    id: int
-    name: str
-    dimensions: Sequence[Dimension]
-    unit: str = None
-    description: str = None
-
-    def __hash__(self):
-        return id(self)
-
-
-@dataclass
-class Value:
-    location: Location
-    variable: Variable
-    value: float
-    dimension_dict: Dict[Dimension, int] = None
-
-    def __hash__(self):
-        return id(self)
-
-
-@dataclass
-class Options:
-    info: Sequence[Info]
-    locations: Sequence[Location]
-    dimensions: Sequence[Dimension]
-    variables: Sequence[Variable]
-    values: Sequence[Value]
-
-    def __hash__(self):
-        return id(self)
-
+from data_sturctures import Info, Location, Dimension, Variable, Value, Options
 
 # %% helpers
-_NEW_LINE_CHARACTER = '\n'
+_NEW_LINE_CHARACTER = "\n"
 
 
 def _open_database(path: str):
@@ -101,29 +31,29 @@ def _execute_sql(db_connection: sqlite3.Connection, sql: str, params: Sequence =
 
 
 def _create_info_table(db_connection: sqlite3.Connection):
-    sql = '''
+    sql = """
         CREATE TABLE info (
             key VARCHAR PRIMARY KEY,
             value TEXT,
             label VARCHAR
         )
-    '''
+    """
     _execute_sql(db_connection, sql)
 
 
 def _create_location_table(db_connection: sqlite3.Connection):
-    sql = '''
+    sql = """
         CREATE TABLE location (
             id INTEGER PRIMARY KEY,
             geometry TEXT,
             metadata TEXT
         )
-    '''
+    """
     _execute_sql(db_connection, sql)
 
 
 def _create_dimension_table(db_connection: sqlite3.Connection):
-    sql = '''
+    sql = """
         CREATE TABLE dimension (
             id INTEGER PRIMARY KEY,
             name VARCHAR NOT NULL,
@@ -131,24 +61,24 @@ def _create_dimension_table(db_connection: sqlite3.Connection):
             description TEXT,
             value_labels TEXT
         )
-    '''
+    """
     _execute_sql(db_connection, sql)
 
 
 def _create_variable_table(db_connection: sqlite3.Connection):
-    sql = '''
+    sql = """
         CREATE TABLE variable (
             id INTEGER PRIMARY KEY,
             name VARCHAR NOT NULL,
             unit VARCHAR,
             description TEXT
         )
-    '''
+    """
     _execute_sql(db_connection, sql)
 
 
 def _create_variable_dimension_table(db_connection: sqlite3.Connection):
-    sql = '''
+    sql = """
         CREATE TABLE variable_dimension (
             variable INTEGER NOT NULL,
             dimension INTEGER NOT NULL,
@@ -156,14 +86,17 @@ def _create_variable_dimension_table(db_connection: sqlite3.Connection):
             FOREIGN KEY (dimension) REFERENCES dimension (id),
             PRIMARY KEY (variable, dimension)
         )
-    '''
+    """
     _execute_sql(db_connection, sql)
 
 
-def _create_value_table(db_connection: sqlite3.Connection, dimensions: Sequence[Dimension]):
+def _create_value_table(
+    db_connection: sqlite3.Connection, dimensions: Sequence[Dimension]
+):
     dimension_column_names = list(
-        map(lambda dimension: f'dimension_{dimension.id}', dimensions))
-    sql = f'''
+        map(lambda dimension: f"dimension_{dimension.id}", dimensions)
+    )
+    sql = f"""
         CREATE TABLE value (
             location INTEGER NOT NULL,
             variable INTEGER NOT NULL,
@@ -172,7 +105,7 @@ def _create_value_table(db_connection: sqlite3.Connection, dimensions: Sequence[
             FOREIGN KEY (variable) REFERENCES variable (id),
             PRIMARY KEY (location, variable{', ' if dimension_column_names is not None else ''}{', '.join(dimension_column_names)})
         )
-    '''
+    """
     _execute_sql(db_connection, sql)
 
 
@@ -186,44 +119,71 @@ def _create_tables(db_connection: sqlite3.Connection, dimensions: Sequence[Dimen
 
 
 def _fill_info_table(db_connection: sqlite3.Connection, info: Sequence[Info]):
-    sql = f'''
+    sql = f"""
         INSERT INTO info (key, value, label) values (?, ?, ?)
-    '''
+    """
     for inf in info:
         _execute_sql(db_connection, sql, [inf.key, inf.value, inf.label])
 
 
-def _fill_location_table(db_connection: sqlite3.Connection, locations: Sequence[Location]):
-    sql = f'''
+def _fill_location_table(
+    db_connection: sqlite3.Connection, locations: Sequence[Location]
+):
+    sql = f"""
         INSERT INTO location (id, geometry, metadata) values (?, ?, ?)
-    '''
+    """
     for location in locations:
-        _execute_sql(db_connection, sql, [
-                     location.id, json.dumps(location.geometry, indent=2), json.dumps(location.metadata, indent=2)])
+        _execute_sql(
+            db_connection,
+            sql,
+            [
+                location.id,
+                json.dumps(location.geometry, indent=2),
+                json.dumps(location.metadata, indent=2),
+            ],
+        )
 
 
-def _fill_dimension_table(db_connection: sqlite3.Connection, dimensions: Sequence[Dimension]):
-    sql = f'''
+def _fill_dimension_table(
+    db_connection: sqlite3.Connection, dimensions: Sequence[Dimension]
+):
+    sql = f"""
         INSERT INTO dimension (id, name, size, description, value_labels) values (?, ?, ?, ?, ?)
-    '''
+    """
     for dimension in dimensions:
-        _execute_sql(db_connection, sql, [
-                     dimension.id, dimension.name, dimension.size, dimension.description, json.dumps(dimension.value_labels)])
+        _execute_sql(
+            db_connection,
+            sql,
+            [
+                dimension.id,
+                dimension.name,
+                dimension.size,
+                dimension.description,
+                json.dumps(dimension.value_labels),
+            ],
+        )
 
 
-def _fill_variable_table(db_connection: sqlite3.Connection, variables: Sequence[Variable]):
-    sql = f'''
+def _fill_variable_table(
+    db_connection: sqlite3.Connection, variables: Sequence[Variable]
+):
+    sql = f"""
         INSERT INTO variable (id, name, unit, description) values (?, ?, ?, ?)
-    '''
+    """
     for variable in variables:
-        _execute_sql(db_connection, sql, [
-                     variable.id, variable.name, variable.unit, variable.description])
+        _execute_sql(
+            db_connection,
+            sql,
+            [variable.id, variable.name, variable.unit, variable.description],
+        )
 
 
-def _fill_variable_dimension_table(db_connection: sqlite3.Connection, variables: Sequence[Variable]):
-    sql = f'''
+def _fill_variable_dimension_table(
+    db_connection: sqlite3.Connection, variables: Sequence[Variable]
+):
+    sql = f"""
         INSERT INTO variable_dimension (variable, dimension) values (?, ?)
-    '''
+    """
     for variable in variables:
         if variable.dimensions is not None:
             for dimension in variable.dimensions:
@@ -234,15 +194,16 @@ def _fill_value_table(db_connection: sqlite3.Connection, values: Sequence[Value]
     for value in values:
         dimensions = list(value.dimension_dict.keys())
         dimension_column_names = list(
-            map(lambda dimension: f'dimension_{dimension.id}', dimensions))
-        sql = f'''
+            map(lambda dimension: f"dimension_{dimension.id}", dimensions)
+        )
+        sql = f"""
             INSERT INTO value (
                 location, 
                 variable, 
                 {f', {_NEW_LINE_CHARACTER}'.join(dimension_column_names)}{', ' if dimension_column_names is not None else ''}
                 value
             ) values (?, ?, {''.join(map(lambda dimension: '?, ', dimension_column_names))} ?)
-        '''
+        """
         params = [value.location.id, value.variable.id]
         for dimension in dimensions:
             params.append(value.dimension_dict[dimension])
@@ -259,14 +220,16 @@ def _fill_tables(db_connection: sqlite3.Connection, options: Options):
     _fill_value_table(db_connection, options.values)
 
 
-def _query_db_table(db_connection: sqlite3.Connection, table_name: str, columns: Sequence[str] = None):
+def _query_db_table(
+    db_connection: sqlite3.Connection, table_name: str, columns: Sequence[str] = None
+):
     db_cursor = db_connection.cursor()
     columns_to_query = columns
     if columns is None:
-        columns_to_query = ['*']
-    sql = f'''
+        columns_to_query = ["*"]
+    sql = f"""
         SELECT {', '.join(columns_to_query)} FROM {table_name}
-    '''
+    """
     db_cursor.execute(sql)
     headers = list(map(lambda d: d[0], db_cursor.description))
     rows = db_cursor.fetchall()
@@ -275,64 +238,118 @@ def _query_db_table(db_connection: sqlite3.Connection, table_name: str, columns:
 
 # %% exported functions
 
+
 def read_gwfvis_db(path: str):
     db_connection = _open_database(path)
-    query_result = _query_db_table(
-        db_connection=db_connection, table_name='info')
-    info = list(map(lambda d: Info(
-        key=d['key'], value=d['value'], label=d['label']), query_result))
-
-    query_result = _query_db_table(
-        db_connection=db_connection, table_name='location')
-    locations = list(map(lambda d: Location(id=d['id'], geometry=json.loads(
-        d['geometry']), metadata=json.loads(d['metadata'])), query_result))
-
-    query_result = _query_db_table(
-        db_connection=db_connection, table_name='dimension')
-    dimensions = list(map(lambda d: Dimension(id=d['id'], name=d['name'], size=d['size'],
-                      description=d['description'], value_labels=json.loads(d['value_labels'])), query_result))
-
-    query_result = _query_db_table(
-        db_connection=db_connection, table_name='variable')
-    variables = list(map(lambda d: Variable(
-        id=d['id'], name=d['name'], unit=d['unit'], description=d['description'], dimensions=[]), query_result))
-
-    query_result = _query_db_table(
-        db_connection=db_connection, table_name='variable_dimension')
-    for q in query_result:
-        variable_id = q['variable']
-        dimension_id = q['dimension']
-        variable = next(
-            filter(lambda variable: variable.id == variable_id, variables), None)
-        dimension = next(
-            filter(lambda dimension: dimension.id == dimension_id, dimensions), None)
-        if (variable is not None and dimension is not None):
-            variable.dimensions.append(dimension)
-
-    query_result = _query_db_table(
-        db_connection=db_connection, table_name='value')
-    values = list(
+    query_result = _query_db_table(db_connection=db_connection, table_name="info")
+    info = list(
         map(
-            lambda value: Value(
-                location=next(filter(lambda location: location.id ==
-                              value['location'], locations), None),
-                variable=next(
-                    filter(lambda variable: variable.id == value['variable'], variables), None),
-                value=value['value'],
-                dimension_dict=dict(
-                    map(
-                        lambda key: (next(dimension for dimension in dimensions if dimension.id == int(
-                            key[10:])), value[key]),
-                        list(filter(lambda key: key.startswith(
-                            'dimension_'), value.keys()))
-                    )
-                )
-            ),
-            query_result
+            lambda d: Info(key=d["key"], value=d["value"], label=d["label"]),
+            query_result,
         )
     )
 
-    return Options(info=info, locations=locations, dimensions=dimensions, variables=variables, values=values)
+    query_result = _query_db_table(db_connection=db_connection, table_name="location")
+    locations = list(
+        map(
+            lambda d: Location(
+                id=d["id"],
+                geometry=json.loads(d["geometry"]),
+                metadata=json.loads(d["metadata"]),
+            ),
+            query_result,
+        )
+    )
+
+    query_result = _query_db_table(db_connection=db_connection, table_name="dimension")
+    dimensions = list(
+        map(
+            lambda d: Dimension(
+                id=d["id"],
+                name=d["name"],
+                size=d["size"],
+                description=d["description"],
+                value_labels=json.loads(d["value_labels"]),
+            ),
+            query_result,
+        )
+    )
+
+    query_result = _query_db_table(db_connection=db_connection, table_name="variable")
+    variables = list(
+        map(
+            lambda d: Variable(
+                id=d["id"],
+                name=d["name"],
+                unit=d["unit"],
+                description=d["description"],
+                dimensions=[],
+            ),
+            query_result,
+        )
+    )
+
+    query_result = _query_db_table(
+        db_connection=db_connection, table_name="variable_dimension"
+    )
+    for q in query_result:
+        variable_id = q["variable"]
+        dimension_id = q["dimension"]
+        variable = next(
+            filter(lambda variable: variable.id == variable_id, variables), None
+        )
+        dimension = next(
+            filter(lambda dimension: dimension.id == dimension_id, dimensions), None
+        )
+        if variable is not None and dimension is not None:
+            variable.dimensions.append(dimension)
+
+    query_result = _query_db_table(db_connection=db_connection, table_name="value")
+    values = list(
+        map(
+            lambda value: Value(
+                location=next(
+                    filter(
+                        lambda location: location.id == value["location"], locations
+                    ),
+                    None,
+                ),
+                variable=next(
+                    filter(
+                        lambda variable: variable.id == value["variable"], variables
+                    ),
+                    None,
+                ),
+                value=value["value"],
+                dimension_dict=dict(
+                    map(
+                        lambda key: (
+                            next(
+                                dimension
+                                for dimension in dimensions
+                                if dimension.id == int(key[10:])
+                            ),
+                            value[key],
+                        ),
+                        list(
+                            filter(
+                                lambda key: key.startswith("dimension_"), value.keys()
+                            )
+                        ),
+                    )
+                ),
+            ),
+            query_result,
+        )
+    )
+
+    return Options(
+        info=info,
+        locations=locations,
+        dimensions=dimensions,
+        variables=variables,
+        values=values,
+    )
 
 
 def generate_gwfvis_db(path: str, options: Options):
